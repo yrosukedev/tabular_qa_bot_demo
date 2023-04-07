ef2a2984a8e342ae9f5a8fdcf68f8d0c7da1cdaa
shrek wang <shrekwang1@163.com>
2023-04-07 16:07:07 +0800

    Try a QA demo with Elastic search and 'deepset/all-mpnet-base-v2-table' embeddings.

--- tabular_qa_keywords/elastic_retriever.py
+++ tabular_qa_keywords/elastic_retriever.py
@@ -1,21 +1,94 @@
 import pandas as pd
+
 from typing import List
+
+import logging
+
+from haystack import Document
+
+from haystack.document_stores import ElasticsearchDocumentStore
+
+from haystack.nodes.retriever import EmbeddingRetriever
 
 
 def flat_table_cell_to_table(table: pd.DataFrame) -> List[pd.DataFrame]:
     # Iterate over the rows of the input DataFrame
-    output = []
-    for i in range(1, len(table)):
-        for j in range(1, len(table.columns)):
-            entity = table.iloc[0, 0]
-            entity_value = table.iloc[i, 0]
-            property_name = table.iloc[0, j]
-            property_value = table.iloc[i, j]
-            # Create a new DataFrame for the entity's property
-            property_df = pd.DataFrame([
-                [entity, property_name],
-                [entity_value, property_value]
-            ])
-            # Append the new DataFrame to the output list
-            output.append(property_df)
+    output = list()
+    for column_label, content in table.items():
+        for index_label, value in content.items():
+            output.append(
+                pd.DataFrame([index_label, value], columns=[column_label]))
     return output
+
+
+def index_data_from_table(doc_path: str, document_index: str) -> None:
+
+    # logging
+    logging.basicConfig(
+        format="%(levelname)s - %(name)s -  %(message)s", level=logging.WARNING)
+    logging.getLogger("haystack").setLevel(logging.INFO)
+
+    # indexing data
+    logging.getLogger("haystack").info("🚧 start to read table as documents")
+    df = pd.read_csv(doc_path, index_col=0)
+    df.fillna(value="", inplace=True)  # Minimal cleaning
+    tables = flat_table_cell_to_table(table=df)
+    documents = [Document(content=table, content_type="table")
+                 for table in tables]
+    logging.getLogger("haystack").info("✅ succeeds to read table as documents")
+
+    # connecting to Elasticsearch
+    logging.getLogger("haystack").info("🚧 start to connect to Elasticsearch")
+    document_store = ElasticsearchDocumentStore(
+        username="admin", password="admin", index=document_index)
+    logging.getLogger("haystack").info(
+        "✅ succeeds to connect to Elasticsearch")
+
+    # add the table to the DocumentStore
+    logging.getLogger("haystack").info(
+        "🚧 start to write documents to Elasticsearch")
+    document_store.write_documents(documents, index=document_index)
+    logging.getLogger("haystack").info(
+        "✅ succeeds to write documents to Elasticsearch")
+
+    # update embeddings
+    retriever = EmbeddingRetriever(
+        document_store=document_store, embedding_model="deepset/all-mpnet-base-v2-table")
+    logging.getLogger("haystack").info("🚧 start to update embeddings")
+    document_store.update_embeddings(retriever=retriever)
+    logging.getLogger("haystack").info("✅ succeeds to update embeddings")
+
+
+def query_table(query: str, document_index: str) -> None:
+
+    # logging
+    logging.basicConfig(
+        format="%(levelname)s - %(name)s -  %(message)s", level=logging.WARNING)
+    logging.getLogger("haystack").setLevel(logging.INFO)
+
+    # connecting to Elasticsearch
+    logging.getLogger("haystack").info("🚧 start to connect to Elasticsearch")
+    document_store = ElasticsearchDocumentStore(
+        username="admin", password="admin", index=document_index)
+    logging.getLogger("haystack").info(
+        "✅ succeeds to connect to Elasticsearch")
+
+    # initialize retriever
+    logging.getLogger("haystack").info(
+        "🚧 start to retrieve relavant tables with Retriever")
+    retriever = EmbeddingRetriever(
+        document_store=document_store, embedding_model="deepset/all-mpnet-base-v2-table")
+    # retrieve relavant documents
+    retrived_tables = retriever.retrieve(query, index=document_index, top_k=5)
+    logging.getLogger("haystack").info(
+        "✅ succeeds to retrieve relavant tables with Retriever")
+    for table in retrived_tables:
+        print(table)
+
+
+if __name__ == "__main__":
+    query = "护眼台灯的库存是多少"
+    doc_path = "~/Downloads/ryosuke_dev_qa_3.csv"
+    document_index = "table_qa.keywords.ryosuke_dev_qa_3.v1"
+    index_data_from_table(doc_path=doc_path, document_index=document_index)
+    query_table(query=query, document_index=document_index)
---
+++ tabular_qa_keywords/elastic_retriever_test.py
@@ -0,0 +1,87 @@
+import unittest
+import elastic_retriever
+import pandas as pd
+from typing import List, TypeVar, Callable, Union
+
+
+T = TypeVar('T')
+
+
+def check_list_equal(list1: List[T], list2: List[T], matcher: Callable[[T, T], bool]) -> bool:
+    for x, y in zip(list1, list2):
+        if not matcher(x, y):
+            return False
+    return True
+
+
+def data_frame_matcher(df1: pd.DataFrame, df2: pd.DataFrame) -> bool:
+    return df1.equals(df2) and df1.index.equals(df2.index) and df1.columns.equals(df2.columns)
+
+
+class TestFlatTableCell(unittest.TestCase):
+
+    def assertDataFrameListEqual(self, xs1: List[pd.DataFrame], xs2: List[pd.DataFrame], msg: Union[str, None] = None) -> None:
+        self.assertTrue(check_list_equal(xs1, xs2, data_frame_matcher), msg)
+
+    def assertDataFrameListNotEqual(self, xs1: List[pd.DataFrame], xs2: List[pd.DataFrame], msg: Union[str, None] = None) -> None:
+        self.assertFalse(check_list_equal(xs1, xs2, data_frame_matcher), msg)
+
+    def test_pandas_basic_usage(self):
+        input = pd.DataFrame([
+            ["Fruit", "Price", "Color"],
+            ["Apple", "10$/KG", "Green"],
+            ["Grep", "5$/KG", "purple"]
+        ])
+        print(f"index: {input.index}")
+        print(f"columns: {input.columns}")
+        print(f"values: {input.values}")
+        print(f"axes: {input.axes}")
+        print(f"ndim: {input.ndim}")
+        print(f"size: {input.size}")
+        print(f"shape: {input.shape}")
+
+    def test_equality_of_two_data_frames(self):
+        x = pd.DataFrame([
+            ["10$/KG", "Green"],
+            ["5$/KG", "purple"]
+        ], columns=["Price", "Color"], index=["Apple", "Grep"])
+        y = pd.DataFrame([
+            ["10$/KG", "Green"],
+            ["5$/KG", "purple"]
+        ], columns=["Price", "Color"], index=["Apple", "Grep"])
+        z = pd.DataFrame([
+            ["10$/KG", "Green"],
+            ["5$/KG", "purple"]
+        ], columns=["Price", "Color X"], index=["Apple", "Grep"])
+
+        self.assertDataFrameListEqual(
+            [x, y], [x, y], "data frame lists should be equal")
+        self.assertDataFrameListNotEqual(
+            [x, y], [x, z], "data frame lists shouldn't be equal")
+
+    def test_flat_table_cell_to_table(self):
+        input = pd.DataFrame([
+            ["10$/KG", "green"],
+            ["5$/KG", "purple"]
+        ], columns=["Price", "Color"], index=["Apple", "Grep"])
+
+        expected_outputs = [
+            pd.DataFrame(["Apple", "10$/KG"], columns=["Price"]),
+            pd.DataFrame(["Grep", "5$/KG"], columns=["Price"]),
+            pd.DataFrame(["Apple", "green"], columns=["Color"]),
+            pd.DataFrame(["Grep", "purple"], columns=["Color"]),
+        ]
+        actual_outputs = elastic_retriever.flat_table_cell_to_table(input)
+        self.assertDataFrameListEqual(
+            expected_outputs, actual_outputs, msg="data frame list should be equal")
+
+    # def test_flat_real_table(self):
+    #     doc_path = "~/Downloads/ryosuke_dev_qa_1.csv"
+    #     input_table = pd.read_csv(doc_path, index_col=0)
+    #     print(f"1st cell from the input table: \n{input_table.head(1)}")
+    #     output_tables = elastic_retriever.flat_table_cell_to_table(input_table)
+    #     print(f"1st output table: \n{output_tables[0]}")
+
+
+if __name__ == "__main__":
+    unittest.main()
